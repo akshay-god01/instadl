29916154b8597ff500dd362cc4ea4ec777d524b9
Akshay <akshayeypz0@gmail.com>
2024-08-22 03:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def insta_downloader():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # Run the app on port 5001
-    app.run(debug=True, port=5001)
+    # Run the app on port 5001 and bind to 0.0.0.0
+    app.run(host='0.0.0.0', port=5001, debug=True)
