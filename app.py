edd25d282cf21f7ce23eac801a170c38bbcac336
Akshay <akshayeypz0@gmail.com>
2024-08-22 03:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-
 from flask import Flask, request, jsonify
 import instaloader
 import re
-import time
-import os
 
 app = Flask(__name__)
 L = instaloader.Instaloader()
-
-# Get Instagram credentials from environment variables
-INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
-INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
-
-# Log in to Instagram (if credentials are provided)
-if INSTAGRAM_USERNAME and INSTAGRAM_PASSWORD:
-    L.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
 
 @app.route('/insta', methods=['GET'])
 def insta_downloader():
@@ -42,9 +31,7 @@ def insta_downloader():
         shortcode = match.group(1)
         print(f"Extracted Shortcode: {shortcode}")
 
-        # Rate limiting
-        time.sleep(2)  # Wait for 2 seconds between requests
-
+        # Access the post data without logging in
         post = instaloader.Post.from_shortcode(L.context, shortcode)
 
         # Collecting data
@@ -86,5 +73,4 @@ def insta_downloader():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # Run the app on port 5001 and bind to 0.0.0.0
     app.run(host='0.0.0.0', port=5001, debug=True)
